05a1583d6c2a0b1d4ac049bc77976015fb19253b
Amila Indika <amilaindika789@gmail.com>
2024-07-08 21:44:47 -1000

    Add password generator script

---
+++ src/pkg/password_generator.py
@@ -0,0 +1,49 @@
+# Password Generator Project
+import random
+import secrets
+import string
+from typing import List
+
+letters = list(string.ascii_letters)
+numbers = list(string.digits)
+symbols = ["!", "#", "$", "%", "&", "(", ")", "*", "+"]
+
+
+def get_user_input(prompt: str) -> int:
+    return int(input(prompt))
+
+
+def append_characters_to_password(character_count: int, characters: List, password: List) -> List:
+    for _ in range(character_count):
+        random_character = secrets.choice(characters)
+        password.append(random_character)
+    return password
+
+
+def shuffle_characters(password: List) -> List:
+    return random.sample(password, len(password))
+
+
+def join_characters(password: List) -> str:
+    return "".join(password)
+
+
+def main():
+    print("Welcome to the Password Generator!")
+    letters_count = get_user_input("How many letters would you like in your password?\n")
+    symbols_count = get_user_input(f"How many symbols would you like?\n")
+    numbers_count = get_user_input(f"How many numbers would you like?\n")
+
+    password_characters = []
+
+    password_characters = append_characters_to_password(letters_count, letters, password_characters)
+    password_characters = append_characters_to_password(symbols_count, symbols, password_characters)
+    password_characters = append_characters_to_password(numbers_count, numbers, password_characters)
+
+    shuffled_password_characters = shuffle_characters(password_characters)
+    secure_password = join_characters(shuffled_password_characters)
+
+    print(f"Your password is: {secure_password}")
+
+if __name__ == "__main__":
+    main()
